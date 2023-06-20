16ef9a3d4b6207fcb7fde51d52680f45ff31e07e
maw <maouiabend@gmail.com>
2023-06-20 02:18:53 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints single digit number of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
