4ca5e285daae8fdabca8269bce6d360ed102649f
maw <maouiabend@gmail.com>
2023-06-21 04:50:57 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,23 @@
-#include"main.h"
-
+#include "main.h"
 /**
- *
- *
+ * print_alphabet_x10 - print a..z 10X
+ * Return: void
  */
 
+void print_alphabet_x10(void)
+{
+	int count = 1;
 
+	while (count <= 10)
+	{
+		char alfa = 'a';
+
+		while (alfa <= 'z')
+		{
+			_putchar(alfa);
+			alfa++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
