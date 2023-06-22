ece50c008d360357388a6a57f6e3360154518f90
maw <maouiabend@gmail.com>
2023-06-22 05:13:36 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @num: int to test it
+ * Return: last digit of last
+ */
+int print_last_digit(int num)
+{
+	int last = num % 10;
+
+	if (last < 0)
+		last *= -1;
+
+	return (last);
+}
+
