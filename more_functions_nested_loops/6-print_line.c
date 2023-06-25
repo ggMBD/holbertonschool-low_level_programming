42ee4d7f8dbbccd97ee86a285b739fbc487a5821
maw <maouiabend@gmail.com>
2023-06-25 13:54:23 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the termina
+ * @n: line size
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n++;
+	}
+	_putchar('\n'};
+}
