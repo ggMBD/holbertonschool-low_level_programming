96c27502b6c66f2e502222857ae0cddd18480b6a
maw <maouiabend@gmail.com>
2023-06-25 11:59:38 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
