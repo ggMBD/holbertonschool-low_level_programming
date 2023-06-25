a5ff8ad0a49de4802a8badbac9d183d902f2a103
maw <maouiabend@gmail.com>
2023-06-25 11:48:13 -0700

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if c is lower case
+ * @c: to check
+ * Return: 1 if c is digit or 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
