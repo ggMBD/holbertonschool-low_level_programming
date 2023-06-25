bcc04712eca9eb52fcbeaea45178db2619724d0b
maw <maouiabend@gmail.com>
2023-06-25 12:11:09 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print from 0 to 9 without 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 4 && i != 6)
+			_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
