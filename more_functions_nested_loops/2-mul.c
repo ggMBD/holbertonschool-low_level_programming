64dce6cde3f697a9903dba2c1816818eaf9c1186
maw <maouiabend@gmail.com>
2023-06-25 11:54:04 -0700

    2œ

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiple 2 nember
+ * @a: intiger
+ * @b: intiger
+ * Return: multiple of a and b
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
