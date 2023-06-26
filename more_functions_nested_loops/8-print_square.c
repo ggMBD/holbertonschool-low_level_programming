89b5c0a36b109fc444498db619500100ebc33c8f
maw <maouiabend@gmail.com>
2023-06-26 01:37:05 -0700

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int size2 = size;
+	while (size > 0)
+	{
+		while (size2 > 0)
+		{
+			_putchar('#' + '0');
+			size2--;
+		}
+	_putchar('\n');
+	size--;
+	}
+}
