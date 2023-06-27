230fd549ce902166814e667b1aa251cf96c8f2ff
maw <maouiabend@gmail.com>
2023-06-27 15:42:21 -0700

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,21 @@ void puts_half(char *str)
 {
 	char *p = str;
 	int len;
-	int half;
+	int half = 0;
 	int index;
-
+	
 	while (*p != '\0')
 	{
 		len++;
 		p++;
 	}
-	if ((len % 2) == 0)
-		half = len / 2;
-	else
-		half = (len - 1) / 2;
+	        if ((len % 2) == 0)
+                half = len / 2;
+        else
+                half = (len - 1) / 2;
 	for (index = half; index < len; index++)
-	{
-		_putchar(str[index]);
-	}
+        {
+                _putchar(str[index]);
+        }
+
 }
