009495e34f499aa5de24704f2e150b20845b2d07
maw <maouiabend@gmail.com>
2023-06-28 07:38:05 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - strcpy
+ * @dest: destination
+ * @src: source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
