2d5e3519300b64d3d866df7514a4e66a5dca482b
maw <maouiabend@gmail.com>
2023-06-28 06:53:02 -0700

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,19 @@ void puts_half(char *str)
 	int len = 0;
 	int half;
 	int index = 0;
-	
+
 	while (*p != '\0')
 	{
 		len++;
 		p++;
 	}
-	        if ((len % 2) == 0)
-                half = len / 2;
-        else
-                half = (len - 1) / 2;
-	for (index = half; index < len; index++)
-        {
-                _putchar(str[index]);
-        }
+	if ((len % 2) == 0)
+		half = len / 2;
+	else
+		half = (len - 1) / 2;
+	for (index = half + 1; index < len; index++)
+	{
+		_putchar(str[index]);
+	}
 	_putchar('\n');
 }
