5ce49b560a177f26f41459cd4bd550eacf3b4fb3
maw <maouiabend@gmail.com>
2023-07-01 10:47:36 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -21,12 +21,12 @@ char *cap_string(char *input)
 			}
 
 			if (input[index - 1] == 32 || input[index - 1] == 9 ||
-			input[index - 1] == 10 ||
+			input[index - 1] == 10 || input[index - 1] == 124 ||
 			input[index - 1] == 44 || input[index - 1] == 59 ||
 			input[index - 1] == 46 || input[index - 1] == 124 ||
 			input[index - 1] == 33 || input[index - 1] == 63 ||
 			input[index - 1] == 34 || input[index - 1] == 40 ||
-			input[index - 1] == 41 || input[index - 1] == 123 ||)
+			input[index - 1] == 41 || input[index - 1] == 123)
 				{
 					input[index] -= 32;
 				}
