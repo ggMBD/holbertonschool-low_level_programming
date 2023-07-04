5f0fa4d5edb90c826673bd244b0d87cc555eecbf
maw <maouiabend@gmail.com>
2023-07-04 01:55:04 -0700

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * Return: lenth s 
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
