85b8c88325a5ad15e34dc7f208d405c7d54494b5
ggMBD <135523461+ggMBD@users.noreply.github.com>
2023-07-11 18:34:49 +0000

    1-strdup.c

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 My first readme
-Create the static library libmy.a
--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,12 @@
 #include <stdlib.h>
-#include <stdio.h>
+/** 
+ * _strdup - returns a pointer
+ * to a newly allocated space in memory
+ * which contains a copy of the string
+ * given as a parameter
+ * @str: string to copy it
+ * Return: cpy
+*/
 char *_strdup(char *str)
 {
     char *cpy;
