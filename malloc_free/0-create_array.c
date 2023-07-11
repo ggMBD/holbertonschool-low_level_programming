284f098afba18bcf3636432397b0b23662dac7a2
maw <maouiabend@gmail.com>
2023-07-11 02:50:52 -0700

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,19 +5,25 @@
  * @c: to initializes the array with it
  * Return: error(NULL) success(ptr)
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
-	ptr =(char *) malloc(size * sizeof(char));
+
+	ptr = (char *) malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
-	if (ptr == NULL)
-		return (NULL);
+
+	ptr[size] = '\0';
 	return (ptr);
 }
