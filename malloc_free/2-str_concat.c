70dd92658505b75685661f058b1576005f4de90c
maw <maouiabend@gmail.com>
2023-07-11 13:48:09 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	int size, i, j;
 
 	if (s1 == NULL)
-		s1[0] = '\0';
+		s1[0] = "";
 	if (s2 == NULL)
-		s2[0] = '\0';
+		s2[0] = "";
 	size = strlen(s1) + strlen(s1);
 
 	cpy = malloc(sizeof(char) * (size + 1));
