03e5711f003c9b07460dd3512569bae1bf1a5ef7
maw <maouiabend@gmail.com>
2023-07-18 03:43:15 -0700

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -7,11 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d->age);
-		free(d);
-	}
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+
 }
