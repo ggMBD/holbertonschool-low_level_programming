04101b66bfc2b9a7e755ef36ad631c1605f3e5f2
maw <maouiabend@gmail.com>
2023-07-20 12:08:16 -0700

    2-int_index.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef F_P
 #define F_P
 #include <stddef.h>
+#include <stdbool.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
