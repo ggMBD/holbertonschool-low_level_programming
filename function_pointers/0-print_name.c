d0b97cbcedf4cfd12c3618b603e6358b3d3b92d2
maw <maouiabend@gmail.com>
2023-07-20 02:41:08 -0700

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to print it
+ * @f: pointer to a function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f (name);
+	return ;
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef F_P
+#define F_P
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
