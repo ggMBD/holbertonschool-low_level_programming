af38d42bf60d7a1da7547bab03a515def707d5e7
maw <maouiabend@gmail.com>
2023-07-22 15:14:28 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,43 +1,64 @@
-#include <stdio.h>
 #include "main.h"
-int square_root_helper(int n, int start, int end);
-/**
- * _sqrt_recursion - find natural square root
- *
- * @n: integer
- *
- * Return: sqrt_reursion
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	return (square_root_helper(n, 0, n));
-}
+
+
 
 /**
- * square_root_helper - helper function to find the square root.
- * @n: the number input.
- * @start: the starting point of search.
- * @end: the ending point of search.
- *
- * Return: the natural square root of @n, or -1.
+ * _sqrt - finds the square root of a value
+ * @prev: previous value
+ * @root: the square value
+ * Return: the square root
  */
-int square_root_helper(int n, int start, int end)
-{
-	int mid;
 
-	if (start <= end)
+int _sqrt(int prev, int root)
+
+{
+
+	if (prev > root)
+
 	{
-		mid = (start + end) / 2;
 
-		if (mid * mid == n)
-			return (mid);
-		if (mid * mid < n)
-			return (square_root_helper(n, mid + 1, end));
+		return (-1);
 
-					return (square_root_helper(n, start, mid - 1));
 	}
-	return (-1);
+
+	else if (prev * prev == root)
+
+	{
+
+		return (prev);
+
+	}
+
+
+
+	return (_sqrt(prev + 1, root));
+
 }
+
+
+
+/**
+ * _sqrt_recursion - recursive square root of a number
+ * @n: the integer
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+
+{
+
+	if (n < 0)
+
+	{
+
+		return (-1);
+
+	}
+
+
+
+	return (_sqrt(1, n));
+
+}
+
 
