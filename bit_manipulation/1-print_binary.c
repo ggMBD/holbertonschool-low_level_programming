b09b78eab1ec0f19f535a9f15b5d8fb0f343869b
maouia <maouiabend@gmail.com>
2023-08-03 15:58:48 +0100

    correct indentation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -2,23 +2,23 @@
 
 void print_binary(unsigned long int n)
 {
-    int num_bits = sizeof(unsigned long int) * 8;
-    int leading_zero = 1;
-    unsigned long int bit;
-    
-    for (int i = num_bits - 1; i >= 0; i--)
-    {
-        bit = (n >> i) & 1;
+	int num_bits = sizeof(unsigned long int) * 8;
+	int leading_zero = 1;
+	unsigned long int bit;
 
-        if (bit == 1 || !leading_zero)
-        {
-            printf("%lu", bit);
-            leading_zero = 0;
-        }
-    }
+	for (int i = num_bits - 1; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
 
-    if (leading_zero)
-    {
-        printf("0");
-    }
+		if (bit == 1 || !leading_zero)
+		{
+			printf("%lu", bit);
+			leading_zero = 0;
+		}
+	}
+
+	if (leading_zero)
+	{
+		printf("0");
+	}
 }
