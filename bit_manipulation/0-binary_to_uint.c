43b9a3955c4f8e785d02fa0982208ce763fef501
maouia <maouiabend@gmail.com>
2023-08-03 13:53:39 +0100

    bit manipulation

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - binary to unsigned int
+ * @b: binary string
+ * Return: error 0
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		result = result * 2 + (*b - '0');
+		b++;
+	}
+
+	return (result);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN
+#define MAIN
+
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
