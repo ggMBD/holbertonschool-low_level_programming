546548121211a64bb78e91bcdcbc049b3f84f2df
maouia <maouiabend@gmail.com>
2023-08-04 10:57:32 +0100

    first linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - function that prints all the elements of a dlistint
+ * @h: list name
+ * Return: node number (counter)
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		printf("%d", h->n);
+
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
