a8db457b6bb3d45bda489da560574e1f472410a4
maouia <maouiabend@gmail.com>
2023-08-04 10:40:15 +0100

    fix description

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * set_bit - function that returns the value of a bit at a given index.
+ * set_bit - function that sets the value of a bit to 1 at a given index
  * @index: is the index
  * @n: nember
  * Return: sucsess 1
