acc9ca3f156a64f1a3602f078e4d3c7abc6ea1ab
maouia <maouiabend@gmail.com>
2023-08-05 20:28:09 +0100

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of numbers
+ * @h: list name
+ * Return: node number (counter)
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int counter = 0;
+
+    if (head == NULL)
+        return (0);
+	while (head)
+	{
+		counter += head->n;
+		head= head->next;
+	}
+	return (counter);
+}
