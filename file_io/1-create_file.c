c2d99fcf3398f772c05288e8e88252c911475d32
maouia <maouiabend@gmail.com>
2023-08-08 19:18:20 +0100

    destroy my code thank's

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,26 +8,21 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	size_t byte = strlen(text_content);
+	size_t byte;
 	int bw, file_fd;
 
 	if (filename == NULL)
 		return (-1);
-	file_fd = open(filename, O_WRONLY | O_TRUNC);
-	if (file_fd == -1)
-	{
-		file_fd = open(filename, O_CREAT | O_WRONLY, 0600);
-		if (file_fd == -1)
-			return (-1);
-	}
-	if (text_content == NULL)
-	{
-		close(file_fd);
-		return (1);
-	}
+
+	if (text_content != NULL)
+		byte = strlen(text_content);
+
+	file_fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	bw = write(file_fd, text_content, byte);
-	if (bw == -1)
+	if (bw == -1 || file_fd == -1)
 		return (-1);
+
 	close(file_fd);
 	return (1);
 }
