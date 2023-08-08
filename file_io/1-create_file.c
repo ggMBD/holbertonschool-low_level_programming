e489c8cd2f0fc3699678cc262a44c7ce0086eef1
maouia <maouiabend@gmail.com>
2023-08-08 19:00:42 +0100

    add mistaces

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,9 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	file_fd = open(filename, O_WRONLY | O_TRUNC, 0600);
 	if (file_fd == -1)
-		return (-1);
+	{
+	    file_fd = open(filename, O_CREAT | O_RDWR, 0600);
+
+	    if (file_fd == -1)
+            return (-1);
+	}
 	bw = write(file_fd, text_content, byte);
 	if (bw == -1)
 		return (-1);
