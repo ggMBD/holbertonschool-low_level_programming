628fddef1bceaf8c22e90de19c19f02109b2abd6
maouia <maouiabend@gmail.com>
2023-08-11 22:44:59 +0100

    hot fix sure

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,17 +34,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-
 	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if (!new_node->key || !new_node->value)
+	if (!new_node->key)
 	{
-		free(new_node->key);
-		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
