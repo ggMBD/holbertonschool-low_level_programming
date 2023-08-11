3f19520202bf02e768865375407e2a89cfabd504
maouia <maouiabend@gmail.com>
2023-08-11 22:14:42 +0100

    add condition

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,18 +6,37 @@
  * @ht: The hash table
  * @key: The key to add/update
  * @value: The value to associate with the key
+ *
  * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node;
+
+
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *current;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		unsigned int i = 0;
+		while (key[i] != '\0' && current->key[i] != '\0' && key[i] == current->key[i])
+			i++;
+		if (key[i] == '\0' && current->key[i] == '\0')
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
