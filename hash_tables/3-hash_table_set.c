d7b9403268b32cac21f133b0092254c943923490
maouia <maouiabend@gmail.com>
2023-08-12 19:53:38 +0100

    remove while loop

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,44 +11,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *current = ht->array[index], *new_node;
-	size_t key_len = strlen(key), i;
+	unsigned long int index = 0;
+	hash_node_t *current, *new_node;
 
 	if (!ht || !key || !*key)
 		return (0);
-	while (current)
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	if (current && strcmp(key, current->key) == 0)
 	{
-		i = 0;
-		while (current->key[i] && i < key_len && current->key[i] == key[i])
-			i++;
-		if (!current->key[i] && i == key_len)
-		{
-			free(current->value);
-			current->value = strdup(value);
-			return (current->value != NULL);
-		}
-		current = current->next;
+		free(current->value);
+		current->value = strdup(value);
+		return (1);
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(new_node);
-		return (0);
-	}
 	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
